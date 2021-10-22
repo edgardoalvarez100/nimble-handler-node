cc902766b319d95c0710b171f18700dc8f844782
Edgardo Alvarez <edgardo@livedesign.org>
2021-10-22 14:09:48 -0500

    “update”

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const path = require("path");
-// let jsonData = require(path.join(__dirname, "..", "data", "rules.conf"))
+
 
 const indexPage = (req, res)=>{
-    let rawData = fs.readFileSync(path.join(__dirname, "..", "data", "rules.conf"));
+    let rawData = fs.readFileSync(path.normalize(process.env.PATH_CONF));
     let conf = JSON.parse(rawData);
-    // res.json({conf});
+
     let {apps} = conf.SyncResponse.RtmpSettings;
     res.json({apps});
 };
